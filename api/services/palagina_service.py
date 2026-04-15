a10e87294b87fa1194fc07236bdf8c1b434ef2be
moliveri1618 <lastiada1@gmail.com>
2026-04-15 19:49:30 +0200

    d

--- api/services/palagina_service.py
+++ api/services/palagina_service.py
@@ -66,21 +66,6 @@ async def run_nuovo_progetto(
     print("release_url:")
     print(RELEASE_URL)
 
-    # Simulate fake worker result
-    # result = {
-    #     "status": "success",
-    #     "message": "Worker bypassed for testing",
-    #     "updated_storage_state": storage_state,
-    # }
-    # result = await palagina_nuovo_progetto_worker(
-    #     payload=payload,
-    #     headless=headless,
-    #     storage_state=storage_state,
-    #     lock_name=PALAGINA_CREATE_LOCK_NAME,
-    #     owner_id='12312',
-    #     release_url=RELEASE_URL,
-    # )
-
     event = {
         "site": "palagina",
         "action": "nuovo_progetto",
@@ -115,61 +100,3 @@ async def run_nuovo_progetto(
 
     return result
 
-
-# PALAGINA_CREATE_LOCK_NAME = "palagina_nuovo_progetto_create"
-# LOCK_LEASE_SECONDS = 120
-# RELEASE_URL = "http://127.0.0.1:8000/locks/release"
-
-# @app.post("/palagina/nuovo-progetto")
-# async def palagina_nuovo_progetto(
-#     payload: NuovoProgettoPayload = Body(..., example=EXAMPLE_NUOVO_PROGETTO),
-#     headless: bool = Query(False),
-#     db: Session = Depends(get_db),
-# ):
-
-#     return await run_nuovo_progetto(db=db, payload=payload, headless=headless)
-#     # storage_state = get_palagina_storage_state(db)
-
-#     # # acquired, owner_id = acquire_lock(
-#     # #     db=db,
-#     # #     lock_name=PALAGINA_CREATE_LOCK_NAME,
-#     # #     lease_seconds=LOCK_LEASE_SECONDS,
-#     # # )
-
-#     # # if not acquired or not owner_id:
-#     # #     raise HTTPException(
-#     # #         status_code=409,
-#     # #         detail="Another Palagina create-project flow is already in progress.",
-#     # #     )
-
-#     # # event = {
-#     # #     "site": "palagina",
-#     # #     "action": "nuovo_progetto",
-#     # #     "payload": payload.model_dump(),
-#     # #     "headless": headless,
-#     # #     "storage_state": storage_state,
-#     # #     "lock": {
-#     # #         "lock_name": PALAGINA_CREATE_LOCK_NAME,
-#     # #         "owner_id": owner_id,
-#     # #         "release_url": RELEASE_URL,
-#     # #     },
-#     # # }
-
-#     # # # export event for local lambda testing
-#     # # with open("event.json", "w", encoding="utf-8") as f:
-#     # #     json.dump(event, f, indent=2, ensure_ascii=False, default=str)
-
-#     # result = await palagina_nuovo_progetto_worker(
-#     #     payload=payload,
-#     #     headless=headless,
-#     #     storage_state=storage_state,
-#     #     lock_name=PALAGINA_CREATE_LOCK_NAME,
-#     #     # owner_id=owner_id,
-#     #     release_url=RELEASE_URL,
-#     # )
-
-#     # updated_storage_state = result.get("updated_storage_state")
-#     # if updated_storage_state is not None:
-#     #     save_palagina_storage_state(db, updated_storage_state)
-
-#     # return result
